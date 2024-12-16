e458ad2fed42433766b5cd906a9a9ab5b6193a6a
haijaa <antkarl11@gmail.com>
2024-12-16 12:53:19 +0100

    asd

--- frontend/cypress/e2e/FirstE2Esmall.cy.ts
+++ frontend/cypress/e2e/FirstE2Esmall.cy.ts
@@ -1,6 +1,7 @@
 describe("template spec", () => {
   it("Testing landingpage and check for paragraphs", () => {
     cy.visit("http://localhost:5173/");
+    cy.wait(2000)
     cy.get("p").should("contain", "Random");
     cy.get("p").should("contain", "Add");
   });
